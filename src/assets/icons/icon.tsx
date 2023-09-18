8327157d04247322248b9249c93c570b93ec9687
Manh Nguyen <manhnvse173470@fpt.edu.vn>
2023-09-18 10:28:55 +0700

    fix bug header

--- src/assets/icons/icon.tsx
+++ src/assets/icons/icon.tsx
@@ -31,20 +31,19 @@ export const UserStatus = ({ styles }: { styles?: CSSProperties }) => (
 );
 
 export const SearchIcon = ({ styles }: { styles?: CSSProperties }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
-        <path
-            d="M11.5 19C15.9183 19 19.5 15.4183 19.5 11C19.5 6.58172 15.9183 3 11.5 3C7.08172 3 3.5 6.58172 3.5 11C3.5 15.4183 7.08172 19 11.5 19Z"
-            stroke={styles?.stroke || '#B0B3B8'}
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        />
-        <path
-            d="M21.85 21L17.5 16.65"
-            stroke={styles?.stroke || '#B0B3B8'}
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        />
+    <svg
+        style={styles}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="#B0B3B8"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+        <circle cx="11" cy="11" r="8" />
+        <line x1="21" y1="21" x2="16.65" y2="16.65" />
     </svg>
 );
