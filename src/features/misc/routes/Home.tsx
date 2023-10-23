93f5e63ea4dab33a76aace173e571956c74fb3e9
Hai An <anplhse173639@fpt.edu.vn>
2023-10-24 00:44:36 +0700

    message

--- src/features/misc/routes/Home.tsx
+++ src/features/misc/routes/Home.tsx
@@ -16,12 +16,43 @@ export default function Home() {
                 </div>
             </div>
             <div className="pt-20">
-            <h1 className="text-left text-white text-5xl font-sans">Client stories</h1></div>
-           <div className="pt-8">
-            <h2 className="text-left text-white text-2xl font-sans">See more reviews on Tattus</h2>
+                <h1 className="text-center text-white text-5xl font-sans mb-5">Client stories</h1></div>
+            <div className="pt-8">
+                <h2 className="text-center text-white-500 mb-10 text-2xl font-sans ">See more reviews on Tattus</h2>
             </div>
-            
-                
+
+            <div className='grid grid-cols-3 gap-x-8 max-w-7xl mx-auto'  >
+                <div>
+                    <div className='h-96 mb-5'>
+                        <img src="https://d1kq2dqeox7x40.cloudfront.net/images/testimonials/0576102b6673880a29f30.png?w=800" alt=''
+                            className='w-full h-full rounded-lg object-cover' />
+                    </div>
+                    <h3 className='text-center font-medium text-lg mb-3'>REALLY EASY TO USE! </h3>
+                    <span className='block text-center text-white-200 text-sm mb-3'> "It was really easy to use. It showed me loads of different examples of artists, so if I didn’t like one style, I could have a good look through for other options.”</span>
+                    <h4 className='text-center font-medium text-lg '> Scarlett</h4>
+                </div>
+
+                <div>
+                    <div className='h-96 mb-5'>
+                        <img src="https://d1kq2dqeox7x40.cloudfront.net/images/testimonials/e0ea195c88149cc9935c4.png?w=800" alt=''
+                            className='w-full h-full rounded-lg object-cover' />
+                    </div>
+                    <h3 className='text-center font-medium text-lg mb-3'> GREAT APP</h3>
+                    <span className='block text-center text-white-200 text-sm mb-8'> “I like Tattoodo because you can see many artists and many different tattoo styles. It’s really easy to use.”</span>
+                    <h4 className='text-center font-medium text-lg '> Daniel</h4>
+                </div>
+
+                <div>
+                    <div className='h-96 mb-5'>
+                        <img src="https://d1kq2dqeox7x40.cloudfront.net/images/testimonials/576102b6673880a29f30d.png?w=800" alt=''
+                            className='w-full h-full rounded-lg object-cover' />
+                    </div>
+                    <h3 className='text-center font-medium text-lg mb-3'> I LOVED THIS EXPERIENCE!</h3>
+                    <span className='block text-center text-white-200 text-sm mb-3'>“Tattoodo made it so simple for me to find an artist that would create a responably priced design for me and someone that I would get along with.” </span>
+                    <h4 className='text-center font-medium text-lg '>Zaynab </h4>
+                </div>
+            </div>
+
             
         </div>
     );
--- src/layouts/components/Footer/Footer.tsx
+++ src/layouts/components/Footer/Footer.tsx
@@ -1,3 +1,31 @@
-export default function Footer() {
-    return <div>Footer</div>;
-}
+import { Facebook, Instagram, Pinterest, Twitter, YouTube } from '@mui/icons-material';
+import { Container, IconButton, Typography, styled } from '@mui/material';
+export default function Footer(){
+const FooterContainer = styled('footer')(({ theme }) => ({
+  backgroundColor: theme.palette.primary.main,
+  color: theme.palette.primary.contrastText,
+  padding: theme.spacing(2),
+  marginTop: 'auto',
+}));
+
+  return (
+    <FooterContainer sx={{ background: "#607d8b"}}>
+      <Container maxWidth="md">
+      <Typography variant="body1" align="center">       
+        SOCIAL MEDIA         
+        </Typography>
+        <Typography variant="body1" align="center">  
+      <IconButton size="large" edge="start" color="inherit" aria-label="logo"><Facebook /></IconButton>
+      <IconButton size="large" edge="start" color="inherit" aria-label="logo"><Instagram /></IconButton>
+      <IconButton size="large" edge="start" color="inherit" aria-label="logo"><Twitter /></IconButton>
+      <IconButton size="large" edge="start" color="inherit" aria-label="logo"><Pinterest /></IconButton>
+      <IconButton size="large" edge="start" color="inherit" aria-label="logo"><YouTube /></IconButton>
+      </Typography>
+        <Typography variant="body1" align="center">       
+        ©Art Tatoo Lover
+        </Typography>
+      </Container>
+    </FooterContainer>
+  );
+};
+
