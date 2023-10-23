a38185df69bdea7f11059e131a813a659a351f8b
Hai An <anplhse173639@fpt.edu.vn>
2023-10-24 01:20:11 +0700

    message

--- src/layouts/components/Footer/Footer.tsx
+++ src/layouts/components/Footer/Footer.tsx
@@ -1,12 +1,13 @@
 import { Facebook, Instagram, Pinterest, Twitter, YouTube } from '@mui/icons-material';
 import { Container, IconButton, Typography, styled } from '@mui/material';
-export default function Footer(){
+export function Footer(){
 const FooterContainer = styled('footer')(({ theme }) => ({
   backgroundColor: theme.palette.primary.main,
   color: theme.palette.primary.contrastText,
   padding: theme.spacing(2),
   marginTop: 'auto',
 }));
+
 
   return (
     <FooterContainer sx={{ background: "#607d8b"}}>
@@ -22,7 +23,7 @@ const FooterContainer = styled('footer')(({ theme }) => ({
       <IconButton size="large" edge="start" color="inherit" aria-label="logo"><YouTube /></IconButton>
       </Typography>
         <Typography variant="body1" align="center">       
-        ©Art Tatoo Lover
+        ©Art Tatto Lover          
         </Typography>
       </Container>
     </FooterContainer>
