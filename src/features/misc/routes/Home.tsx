ff7746d191daa47b5193e7b5f9fe8a3386f93dff
Manh Nguyen <manhnvse173470@fpt.edu.vn>
2023-10-24 10:20:09 +0700

    fix confilct

--- src/features/misc/routes/Home.tsx
+++ src/features/misc/routes/Home.tsx
@@ -21,39 +21,57 @@ export default function Home() {
                 <h2 className="text-center text-white mb-10 text-xl font-light ">See more reviews on Tattus</h2>
             </div>
 
-            <div className='grid grid-cols-3 gap-x-8 max-w-7xl mx-auto'  >
+            <div className="grid grid-cols-3 gap-x-8 max-w-7xl mx-auto">
                 <div>
-                    <div className='h-96 mb-5'>
-                        <img src="https://d1kq2dqeox7x40.cloudfront.net/images/testimonials/0576102b6673880a29f30.png?w=800" alt=''
-                            className='w-full h-full rounded-lg object-cover' />
+                    <div className="h-96 mb-5">
+                        <img
+                            src="https://d1kq2dqeox7x40.cloudfront.net/images/testimonials/0576102b6673880a29f30.png?w=800"
+                            alt=""
+                            className="w-full h-full rounded-lg object-cover"
+                        />
                     </div>
-                    <h3 className='text-center font-medium text-lg mb-3'>REALLY EASY TO USE! </h3>
-                    <span className='block text-center text-white-200 text-sm mb-3'> "It was really easy to use. It showed me loads of different examples of artists, so if I didn’t like one style, I could have a good look through for other options.”</span>
-                    <h4 className='text-center font-medium text-lg '> Scarlett</h4>
+                    <h3 className="text-center font-medium text-lg mb-3">REALLY EASY TO USE! </h3>
+                    <span className="block text-center text-white-200 text-sm mb-3">
+                        {' '}
+                        "It was really easy to use. It showed me loads of different examples of artists, so if I didn’t
+                        like one style, I could have a good look through for other options.”
+                    </span>
+                    <h4 className="text-center font-medium text-lg "> Scarlett</h4>
                 </div>
 
                 <div>
-                    <div className='h-96 mb-5'>
-                        <img src="https://d1kq2dqeox7x40.cloudfront.net/images/testimonials/e0ea195c88149cc9935c4.png?w=800" alt=''
-                            className='w-full h-full rounded-lg object-cover' />
+                    <div className="h-96 mb-5">
+                        <img
+                            src="https://d1kq2dqeox7x40.cloudfront.net/images/testimonials/e0ea195c88149cc9935c4.png?w=800"
+                            alt=""
+                            className="w-full h-full rounded-lg object-cover"
+                        />
                     </div>
-                    <h3 className='text-center font-medium text-lg mb-3'> GREAT SERVICE</h3>
-                    <span className='block text-center text-white-200 text-sm mb-8'> “I like Tattus because you can see many artists and many different tattoo styles. It’s really easy to use.”</span>
-                    <h4 className='text-center font-medium text-lg '> Daniel</h4>
+                    <h3 className="text-center font-medium text-lg mb-3"> GREAT SERVICE</h3>
+                    <span className="block text-center text-white-200 text-sm mb-8">
+                        {' '}
+                        “I like Tattus because you can see many artists and many different tattoo styles. It’s really
+                        easy to use.”
+                    </span>
+                    <h4 className="text-center font-medium text-lg "> Daniel</h4>
                 </div>
 
                 <div>
-                    <div className='h-96 mb-5'>
-                        <img src="https://d1kq2dqeox7x40.cloudfront.net/images/testimonials/576102b6673880a29f30d.png?w=800" alt=''
-                            className='w-full h-full rounded-lg object-cover' />
+                    <div className="h-96 mb-5">
+                        <img
+                            src="https://d1kq2dqeox7x40.cloudfront.net/images/testimonials/576102b6673880a29f30d.png?w=800"
+                            alt=""
+                            className="w-full h-full rounded-lg object-cover"
+                        />
                     </div>
-                    <h3 className='text-center font-medium text-lg mb-3'> I LOVED THIS EXPERIENCE!</h3>
-                    <span className='block text-center text-white-200 text-sm mb-3'>“Tattus made it so simple for me to find an artist that would create a responably priced design for me and someone that I would get along with.” </span>
-                    <h4 className='text-center font-medium text-lg '>Zaynab </h4>
+                    <h3 className="text-center font-medium text-lg mb-3"> I LOVED THIS EXPERIENCE!</h3>
+                    <span className="block text-center text-white-200 text-sm mb-3">
+                        “Tattus made it so simple for me to find an artist that would create a responably priced design
+                        for me and someone that I would get along with.”{' '}
+                    </span>
+                    <h4 className="text-center font-medium text-lg ">Zaynab </h4>
                 </div>
             </div>
-
-
         </div>
     );
 }
