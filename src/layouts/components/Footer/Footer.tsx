3219684294520bf5a59ac5122e7a07b52c12c8fd
Manh Nguyen <manhnvse173470@fpt.edu.vn>
2023-10-24 10:20:32 +0700

    fix confilct

--- src/layouts/components/Footer/Footer.tsx
+++ src/layouts/components/Footer/Footer.tsx
@@ -1,32 +1,30 @@
-import { Facebook, Instagram, Pinterest, Twitter, YouTube } from '@mui/icons-material';
-import { Container, IconButton, Typography, styled } from '@mui/material';
-export  default function Footer(){
-const FooterContainer = styled('footer')(({ theme }) => ({
-  backgroundColor: theme.palette.primary.main,
-  color: theme.palette.primary.contrastText,
-  padding: theme.spacing(2),
-  marginTop: 'auto',
-}));
+// import { Facebook, Instagram, Pinterest, Twitter, YouTube } from '@mui/icons-material';
+// import { Container, IconButton, Typography, styled } from '@mui/material';
+// export  default function Footer(){
+// const FooterContainer = styled('footer')(({ theme }) => ({
+//   backgroundColor: theme.palette.primary.main,
+//   color: theme.palette.primary.contrastText,
+//   padding: theme.spacing(2),
+//   marginTop: 'auto',
+// }));
 
-
-  return (
-    <FooterContainer sx={{ background: "#607d8b"}}>
-      <Container maxWidth="md">
-      <Typography variant="body1" align="center">       
-        SOCIAL MEDIA         
-        </Typography>
-        <Typography variant="body1" align="center">  
-      <IconButton size="large" edge="start" color="inherit" aria-label="logo"><Facebook /></IconButton>
-      <IconButton size="large" edge="start" color="inherit" aria-label="logo"><Instagram /></IconButton>
-      <IconButton size="large" edge="start" color="inherit" aria-label="logo"><Twitter /></IconButton>
-      <IconButton size="large" edge="start" color="inherit" aria-label="logo"><Pinterest /></IconButton>
-      <IconButton size="large" edge="start" color="inherit" aria-label="logo"><YouTube /></IconButton>
-      </Typography>
-        <Typography variant="body1" align="center">       
-        ©Art Tatto Lover          
-        </Typography>
-      </Container>
-    </FooterContainer>
-  );
-};
-
+//   return (
+//     <FooterContainer sx={{ background: "#607d8b"}}>
+//       <Container maxWidth="md">
+//       <Typography variant="body1" align="center">
+//         SOCIAL MEDIA
+//         </Typography>
+//         <Typography variant="body1" align="center">
+//       <IconButton size="large" edge="start" color="inherit" aria-label="logo"><Facebook /></IconButton>
+//       <IconButton size="large" edge="start" color="inherit" aria-label="logo"><Instagram /></IconButton>
+//       <IconButton size="large" edge="start" color="inherit" aria-label="logo"><Twitter /></IconButton>
+//       <IconButton size="large" edge="start" color="inherit" aria-label="logo"><Pinterest /></IconButton>
+//       <IconButton size="large" edge="start" color="inherit" aria-label="logo"><YouTube /></IconButton>
+//       </Typography>
+//         <Typography variant="body1" align="center">
+//         ©Art Tatto Lover
+//         </Typography>
+//       </Container>
+//     </FooterContainer>
+//   );
+// };
