2507da0ac103b098114a3f1d83be6722f105e9a6
Manh Nguyen <manhnvse173470@fpt.edu.vn>
2023-10-31 20:57:35 +0700

    updload edit studio

--- dashboard/src/features/system/routes/SystemManageStudio.tsx
+++ dashboard/src/features/system/routes/SystemManageStudio.tsx
@@ -1,9 +1,20 @@
-import { Suspense, lazy } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import { Text, useMantineTheme } from '@mantine/core';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const ManageStudios = lazy(() => import('@/features/system/components/ManageStudios'));
 export default function SystemManageStudio() {
     const theme = useMantineTheme();
+    const navigate = useNavigate();
+    const location = useLocation();
+    useEffect(() => {
+        if (location.state && location.state.message) {
+            toast(location.state.message, { type: 'error', theme: 'dark' });
+            navigate('/system/manage-studios', { replace: true });
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     return (
         <div className="flex flex-col gap-y-5">
             <Text className="text-2xl font-semibold" c={theme.colors[theme.primaryColor][6]}>
--- dashboard/src/layouts/StudioLayout/StudioLayout.tsx
+++ dashboard/src/layouts/StudioLayout/StudioLayout.tsx
@@ -1,8 +1,27 @@
 import { AppShell } from '@mantine/core';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { StudioNav } from '../components/NavBar';
+import { useAuthStore } from '@/store/authStore';
+import { useEffect } from 'react';
+import { resetAuthStore } from '@/lib/helper';
 
 export default function StudioLayout() {
+    const { accountType, reset } = useAuthStore();
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (accountType && !accountType.studioId) {
+            if (accountType.role!.id <= 2)
+                navigate('/system/manage-studios', {
+                    replace: true,
+                    state: { message: 'Vui lòng chọn studio để chỉnh sửa trước' },
+                });
+            else {
+                resetAuthStore();
+                reset();
+            }
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [accountType?.studioId]);
     return (
         <>
             <AppShell.Navbar p="md" className="flex flex-col justify-between">
