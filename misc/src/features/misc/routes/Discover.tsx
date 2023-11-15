0aa0c1fe184411f8ff9d3483923def9eb47d1b83
Hai An <anplhse173639@fpt.edu.vn>
2023-11-15 09:46:57 +0700

    footer and discover

--- misc/src/features/misc/routes/Discover.tsx
+++ misc/src/features/misc/routes/Discover.tsx
@@ -1,3 +1,45 @@
+import Slider1 from '@/assets/img/sliderHome1.png';
 export default function Discover() {
-    return <div>Discover</div>;
+    return (
+        <div id="content" className="pt-6 w-full h-[1200px]">
+            <div className="mx-auto w-full mb-16 xl:w-[1372px]">
+                <div className="h-[504px] relative" style={{ backgroundImage: `url(${Slider1})` }}>
+                    <div className="absolute top-11 w-full font-serif-slide text-white text-center">
+                        <h1 className="text-[64px] mb-2">Chào mừng đến với Tattus</h1>
+                    </div>
+                </div>
+            </div>
+            <div className="container mx-auto mt-10 px-4 md:px-0">
+                <h2 className="text-4xl font-bold mb-5 text-center">Dịch vụ của chúng tôi</h2>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                    <div className="bg-transparent p-5 rounded shadow">
+                        <div className="h-96 mb-12 transition-all duration-300 ease-in-out hover:scale-110">
+                            <img src="https://d1kq2dqeox7x40.cloudfront.net/images/posts/postImage_iCaemJe7me.png?w=600" alt="" className="w-full h-full rounded-lg object-cover " />
+                            <h3 className="text-xl font-bold text-center mt-6">Hình xăm cá nhân</h3>
+                            <p className="mt-4 text-gray-500 text-center font-medium">Chúng tôi cung cấp dịch vụ hình xăm cá nhân với nhiều mẫu mã đa dạng.</p>
+                        </div>
+                    </div>
+                    <div className="bg-transparent p-5 rounded shadow">
+                        <div className="h-96 mb-12 transition-all duration-300 ease-in-out hover:scale-110">
+                            <img src="https://d1kq2dqeox7x40.cloudfront.net/images/posts/20180925_lpFNYCbzXR77T1U.png?w=600" alt="" className="w-full h-full rounded-lg object-cover " />
+                            <h3 className="text-xl font-bold text-center mt-6">Hình xăm theo yêu cầu</h3>
+                            <p className="mt-4 text-gray-500 text-center font-medium">Bạn có thể yêu cầu hình xăm theo ý muốn của mình.</p>
+                        </div>
+                    </div>
+                    <div className="bg-transparent p-5 rounded shadow">
+                        <div className="h-96 mb-12 transition-all duration-300 ease-in-out hover:scale-110">
+                            <img src="https://morgansinktattoo.com/wp-content/uploads/2023/03/tattoo2.jpg" alt="" className="w-full h-full rounded-lg object-cover " />
+                            <h3 className="text-xl font-bold text-center mt-6">Tư vấn hình xăm</h3>
+                            <p className="mt-4 text-gray-500 text-center font-medium">Chúng tôi cung cấp dịch vụ tư vấn hình xăm để bạn có thể lựa chọn được hình xăm phù hợp nhất.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
+
+
+
+
--- misc/src/features/misc/routes/Home.tsx
+++ misc/src/features/misc/routes/Home.tsx
@@ -16,62 +16,90 @@ export default function Home() {
                 </div>
             </div>
             {/* An code từ đây */}
-            <div className="pt-12">
-                <h1 className="text-center text-white text-4xl font-semibold mb-5">Client stories</h1>
-                <h2 className="text-center text-white mb-10 text-xl font-light ">See more reviews on Tattus</h2>
+            <div className="pt-20">
+                <h1 className="text-center text-white text-4xl font-semibold mb-5">Câu chuyện khách hàng</h1>
+                <h2 className="text-center text-white mb-10 text-xl font-light ">Xem nhiều đánh giá hơn ở Tattus</h2>
             </div>
 
             <div className="grid grid-cols-3 gap-x-8 max-w-7xl mx-auto">
                 <div>
-                    <div className="h-96 mb-5">
+                    <div className="h-96 mb-5 transition-all duration-300 ease-in-out hover:scale-90">
                         <img
                             src="https://d1kq2dqeox7x40.cloudfront.net/images/testimonials/0576102b6673880a29f30.png?w=800"
                             alt=""
-                            className="w-full h-full rounded-lg object-cover"
+                            className="w-full h-full rounded-lg object-cover "
                         />
                     </div>
-                    <h3 className="text-center font-medium text-lg mb-3">REALLY EASY TO USE! </h3>
-                    <span className="block text-center text-white-200 text-sm mb-3">
+                    <h3 className="text-center font-medium text-lg mb-3">THỰC SỰ DỄ DÀNG SỬ DỤNG ! </h3>
+                    <span className="block text-center  font-medium text-white-200 text-sm mb-2">
                         {' '}
-                        "It was really easy to use. It showed me loads of different examples of artists, so if I didn’t
-                        like one style, I could have a good look through for other options.”
+                        "Nó thực sự dễ sử dụng. Nó cho tôi thấy vô số ví dụ khác nhau về các nghệ sĩ, vì vậy nếu tôi không thích một phong cách, tôi có thể xem qua các lựa chọn khác.”
                     </span>
-                    <h4 className="text-center font-medium text-lg "> Scarlett</h4>
+                    <h4 className="text-center font-medium text-lg mb-24"> Scarlett</h4>
                 </div>
 
                 <div>
-                    <div className="h-96 mb-5">
+                    <div className="h-96 mb-5 transition-all duration-300 ease-in-out hover:scale-90">
                         <img
                             src="https://d1kq2dqeox7x40.cloudfront.net/images/testimonials/e0ea195c88149cc9935c4.png?w=800"
                             alt=""
-                            className="w-full h-full rounded-lg object-cover"
+                            className="w-full h-full rounded-lg object-cover "
                         />
                     </div>
-                    <h3 className="text-center font-medium text-lg mb-3"> GREAT SERVICE</h3>
-                    <span className="block text-center text-white-200 text-sm mb-8">
+                    <h3 className="text-center font-medium text-lg mb-3">DỊCH VỤ TUYỆT VỜI</h3>
+                    <span className="block text-center  font-medium text-white-200 text-sm mb-7">
                         {' '}
-                        “I like Tattus because you can see many artists and many different tattoo styles. It’s really
-                        easy to use.”
+                        “Tôi thích Tattus vì bạn có thể thấy nhiều nghệ sĩ và nhiều phong cách xăm khác nhau. Nó thực sự dễ sử dụng.”
                     </span>
-                    <h4 className="text-center font-medium text-lg "> Daniel</h4>
+                    <h4 className="text-center font-medium text-lg mb-24 "> Daniel</h4>
                 </div>
 
                 <div>
-                    <div className="h-96 mb-5">
+                    <div className="h-96 mb-5 transition-all duration-300 ease-in-out hover:scale-90">
                         <img
                             src="https://d1kq2dqeox7x40.cloudfront.net/images/testimonials/576102b6673880a29f30d.png?w=800"
                             alt=""
-                            className="w-full h-full rounded-lg object-cover"
+                            className="w-full h-full rounded-lg object-cover "
                         />
                     </div>
-                    <h3 className="text-center font-medium text-lg mb-3"> I LOVED THIS EXPERIENCE!</h3>
-                    <span className="block text-center text-white-200 text-sm mb-3">
-                        “Tattus made it so simple for me to find an artist that would create a responably priced design
-                        for me and someone that I would get along with.”{' '}
+                    <h3 className="text-center font-medium text-lg mb-3"> TÔI YÊU TRẢI NGHIỆM NÀY !</h3>
+                    <span className="block text-center font-medium text-white-200 text-sm mb-1">
+                        {' '}“Tattus đã giúp tôi dễ dàng tìm được một nghệ sĩ có thể tạo ra một thiết kế có mức giá phù hợp cho tôi và một người nào đó mà tôi có thể hợp tác.”
                     </span>
-                    <h4 className="text-center font-medium text-lg ">Zaynab </h4>
+                    <h4 className="text-center font-medium text-lg mb-24 ">Zaynab </h4>
                 </div>
+            </div>
+
+            <div className="mx-auto w-full xl:w-[900px] pt-24 " >
+                <div>
+                    <div className="h-96 mb-12 transition-all duration-300 ease-in-out hover:scale-110">
+                        <img
+                            src="https://d1kq2dqeox7x40.cloudfront.net/web/front/inspire.png?w=800"
+                            alt=""
+                            className="w-full h-full rounded-lg object-cover "
+                        />
+                    </div>
+                    <h3 className="text-center font-medium text-2xl mb-8"> Lấy cảm hứng cho hình xăm tiếp theo của bạn.</h3>
+                    <span className="block text-center font-medium text-gray-200 text-sm mb-1">
+                        {' '}Chúng tôi đã tuyển chọn một bộ sưu tập các hình xăm đẹp để bạn phám phá.
+                    </span>
+                    <div className="flex justify-center">
+                        <button className="inline-button justify-center py-3 px-10 m-5  text-white bg-[gray] hover:bg-gray-400 rounded-lg transition-colors">Tatto gallery</button>
+                    </div>
+                </div>
+            </div>
+            <div className="mx-auto w-full xl:w-[900px] pt-24 " >
+                <h1 className="text-[64px] mb-2 text-center font-bold">Tatto Gallery</h1>
+                <h2 className="text-[16px] mb-2 text-center font-medium mt-10">Tất cả hình ảnh trong bộ sưu tập đều là ảnh xăm thực tế được chụp bởi các nghệ sĩ xăm hình của Tattus</h2>
+
+                <div className=" flex h-96 mb-12 transition-all duration-300 ease-in-out hover:scale-110 ">
+                            <img src="https://morgansinktattoo.com/wp-content/uploads/2023/11/8.png" alt="" className="w-2/6 h-full rounded-lg object-cover mt-10 mx-3.5 " />
+                            <img src="https://morgansinktattoo.com/wp-content/uploads/2023/11/2.png" alt="" className="w-2/6 h-full rounded-lg object-cover mt-10 mx-3.5" />
+                            <img src="https://morgansinktattoo.com/wp-content/uploads/2023/11/14.png" alt="" className="w-2/6 h-full rounded-lg object-cover mt-10 mx-3.5 " />
+                        </div>
             </div>
         </div>
     );
 }
+//<button className="inline-button py-3 px-10 m-5  text-white bg-[#6A5AF9] hover:bg-red-400 rounded-lg transition-colors">Tatto gallery</button>
+//  <input type="text" className="py-3 px-10 border border-[#ccc]"/>
