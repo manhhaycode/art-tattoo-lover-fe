0b4c3e087135137052693cf15319e6d99ebf7f1d
Hai An <anplhse173639@fpt.edu.vn>
2023-11-28 14:56:25 +0700

    frot-end update

--- misc/src/features/misc/routes/BecomeStudio.tsx
+++ misc/src/features/misc/routes/BecomeStudio.tsx
@@ -1,9 +1,7 @@
-// An code phần này muốn đổi gì thì đổi
 import Loading from '@/components/Loading';
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 
 const BecomeStudioForm = lazy(() => import('@/features/studios/components/BecomeStudio'));
-import { Suspense } from 'react';
 
 export default function BecomeStudio() {
     return (
